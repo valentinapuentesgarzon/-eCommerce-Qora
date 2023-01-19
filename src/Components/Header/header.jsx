77e48007d41670cf624efcac71ece374e1a15f67
valentinapuentesgarzon <valentinapuentes@gmail.com>
2023-01-19 14:50:53 -0500

    Link saections

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -9,6 +9,8 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 
 import './header.css'
 
+import Logo from '../../assets/img/Marca/Logo-sin-fondo.png'
+
 function Header() {
   return (
     <>
@@ -18,7 +20,7 @@ function Header() {
       {['md'].map((expand) => (
         <Navbar key={expand} bg="light" expand={expand} className="mb-3">
           <Container fluid>
-            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+            <Navbar.Brand href="#"><img src={Logo} alt="" /></Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
@@ -27,26 +29,16 @@ function Header() {
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
+                  Menú
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
-                  <NavDropdown
-                    title="Dropdown"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                  >
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
+                  <Nav.Link href="#action1">Quénes Somos</Nav.Link>
+                  <Nav.Link href="#action2">Productos</Nav.Link>
+                  <Nav.Link href="#action2">Cómo comprar</Nav.Link>
+                  <Nav.Link href="#action2">Contacto</Nav.Link>
+                  
                 </Nav>
                 <Form className="d-flex">
                   <Form.Control
