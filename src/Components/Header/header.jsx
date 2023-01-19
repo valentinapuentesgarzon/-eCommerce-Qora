dd7e485cf7c1c23bac6ef0aad1565095f8d56a13
valentinapuentesgarzon <valentinapuentes@gmail.com>
2023-01-18 23:18:17 -0500

    Add styles and header component

---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './header.css'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Logo from '../../assets/img/Header/Logo-Qora.png'
+
+
+function Header() {
+  return (
+    <>
+      {['md'].map((expand) => (
+        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+          <Container fluid>
+            <img src={Logo} alt=''/>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Quiénes Somos</Nav.Link>
+                  <Nav.Link href="#action2">Productos</Nav.Link>
+                  <Nav.Link href="#action2">Cómo comprar</Nav.Link>
+                  <Nav.Link href="#action2">Contacto</Nav.Link>
+                </Nav>
+                
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default Header;
